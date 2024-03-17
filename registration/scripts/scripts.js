81c1f4562a223c57963aca8478ef07d87670a2fb
группы стационара <st@itstep.lan>
2024-03-17 19:36:47 +0300

    checkregv1

--- registration/scripts/scripts.js
+++ registration/scripts/scripts.js
@@ -1,17 +1,38 @@
 const regButton = document.querySelector("#sendbut"),
-checkPassword = document.querySelector("#check"),
+checkPassword = document.querySelector("#checkpass"),
+checkLogin = document.querySelector("#checklog"),
 password1 = document.querySelector(".same1"),
-password2 = document.querySelector(".same2");
+password2 = document.querySelector(".same2"),
+login = document.querySelector(".login");
 
-window.addEventListener("load",function(){
-    regButton.addEventListener("click",function(){
-        if(password1.value!=password2.value){
-            password1.value = "";
-            password2.value = "";
-            checkPassword.innerHTML = "Пароли не совпадают";
-        }
-        else{
-            checkPassword.innerHTML = "";
-        }
-    });
-})
+
+
+function sendForm(){
+    let loginTemplate = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/g;
+
+    if(password1.value!=password2.value){
+        password1.value = "";
+        password2.value = "";
+        checkPassword.innerHTML = "Пароли не совпадают";
+    } else if(!loginTemplate.test(login.value)){
+        checkLogin.innerHTML = "Неверный логин";
+    }
+    else{
+        const currentLogin = login.value;
+        const currentPassword = password1.value;
+        const currentConfPassword = password2.value;
+        const user = {
+        login: currentLogin,
+        password: currentPassword,
+        confPassword: currentConfPassword
+        };
+        checkPassword.innerHTML = "";
+        checkLogin.innerHTML = "";
+
+        login.value = "";
+        password1.value = "";
+        password2.value = "";
+
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+}
